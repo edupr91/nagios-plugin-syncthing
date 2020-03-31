6fd2dc9ef14bdf6dd98b71ed9b94d7466b618429
David Castañeda <edupr91@gmail.com>
2020-03-31 10:11:40 +0200

    make it python2 & python3 compatible

--- check_syncthing.py
+++ check_syncthing.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # Main Author
 #   - David Castaneda <edupr91@gmail.com>
@@ -28,19 +28,19 @@ args = vars(parser.parse_args())
 
 
 def check_status(http_endpoint, headers):
-    url = f"{http_endpoint}/rest/system/status"
+    url = '{}/rest/system/status'.format(http_endpoint)
     try:
         resp = requests.get(url=url, headers=headers)
         data = resp.json()
     except Exception:
         print('CRITICAL: Error while getting Connection')
         sys.exit(2)
-    print(f"OK: Syncthing is running.  {data['uptime']} secs uptime")
+    print('OK: Syncthing is running. {} secs uptime'.format(data['uptime']))
     sys.exit(0)
 
 
 def get_id(http_endpoint, headers):
-    url = f"{http_endpoint}/rest/system/status"
+    url = '{}/rest/system/status'.format(http_endpoint)
     try:
         resp = requests.get(url=url, headers=headers)
         data = resp.json()
@@ -51,7 +51,7 @@ def get_id(http_endpoint, headers):
 
 
 def check_folder_lc(http_endpoint, headers):
-    url = f"{http_endpoint}/rest/stats/folder"
+    url = '{}/rest/stats/folder'.format(http_endpoint)
     try:
         resp = requests.get(url=url, headers=headers)
         data = resp.json()
@@ -82,24 +82,25 @@ def check_folder_lc(http_endpoint, headers):
     if 0 != len(folder_critical):
         folder_critical_str = separator.join(folder_critical)
         print(
-            f'CRITICAL: we have this/these folder(s) with Errors'
-            f'{folder_critical_str}')
+            'CRITICAL: we have this/these folder(s) with Errors'
+            ' {}'.format(folder_critical_str))
         sys.exit(2)
     elif 0 != len(folder_warning):
         folder_warning_str = separator.join(folder_warning)
         print(
-            f'WARNING: we have this/these folder(s) with Errors'
-            f' {folder_warning_str}')
+            'WARNING: we have this/these folder(s) with Errors'
+            ' {}'.format(folder_warning_str))
         sys.exit(1)
     else:
         folder_ok_str = separator.join(folder_ok)
-        print(f'OK: all good with this/these folder(s) {folder_ok_str}')
+        print('OK: all good with this/these folder(s)'
+              '{}'.format(folder_ok_str))
         sys.exit(0)
 
 
 def check_devices(http_endpoint, headers):
     myID = get_id(http_endpoint, headers)
-    url = f"{http_endpoint}/rest/stats/device"
+    url = '{}/rest/stats/device'.format(http_endpoint)
     try:
         resp = requests.get(url=url, headers=headers)
         data = resp.json()
@@ -112,7 +113,7 @@ def check_devices(http_endpoint, headers):
     device_ok = []
     for device in data:
         # No need to check the last time you the server has seen himself
-        # it always print "1970-01-01T00:00:00Z"
+        # it always print '1970-01-01T00:00:00Z'
         if device == myID:
             continue
         last_scan_str = data[device]['lastSeen'][:-4]
@@ -136,21 +137,21 @@ def check_devices(http_endpoint, headers):
     device_ok_len = len(device_ok)
 
     if 0 != device_critical_len:
-        print(f'CRITICAL: {device_critical_len} device(s) that haven\'t '
-              f'been seen in more than 10 min')
+        print('CRITICAL: {} device(s) that haven\'t '
+              'been seen in more than 10 min'.format(device_critical_len))
         sys.exit(2)
     elif 0 != device_warning_len:
-        print(f'WARNING: {device_warning_len} device(s) that haven\'t been '
-              f'seen in the last 5 min')
+        print('WARNING: {} device(s) that haven\'t been '
+              'seen in the last 5 min'.format(device_warning_len))
         sys.exit(1)
 
     else:
-        print(f'OK: {device_ok_len} devices have been seen lately')
+        print('OK: {} devices have been seen lately'.format(device_ok_len))
         sys.exit(0)
 
 
 def check_folder_status(http_endpoint, headers):
-    url = f"{http_endpoint}/rest/stats/folder"
+    url = '{}/rest/stats/folder'.format(http_endpoint)
     try:
         resp = requests.get(url=url, headers=headers)
         data = resp.json()
@@ -162,28 +163,34 @@ def check_folder_status(http_endpoint, headers):
     folder_warning = []
     for folder in data:
         # get folder status in db
-        folder_url = f"{http_endpoint}/rest/db/status?folder={folder}"
+        folder_url = '{}/rest/db/status?folder=' \
+                     '{}'.format(http_endpoint, folder)
         folder_resp = requests.get(url=folder_url, headers=headers)
         folder_status = folder_resp.json()
 
         if folder_status['errors'] > 0:
-            folder_critical += [f"{folder}:{folder_status['errors']}"]
+            folder_critical += ['{}:{}'.format(
+                folder, folder_status['errors'])]
         elif folder_status['pullErrors'] > 0:
-            folder_critical += [f"{folder}:{folder_status['pullErrors']}"]
+            folder_critical += ['{}:{}'.format(
+                folder, folder_status['pullErrors'])]
         elif folder_status['needBytes'] > 0:
-            folder_warning += [f"{folder}:{folder_status['needBytes']}"]
+            folder_warning += ['{}:{}'.format(
+                folder, folder_status['needBytes'])]
 
     separator = ', '
     if len(folder_critical) > 0:
         folder_critical_str = separator.join(folder_critical)
-        print(f'CRITICAL: folder_id:number_of_error - {folder_critical_str}')
+        print('CRITICAL: folder_id:number_of_error - '
+              '{}'.format(folder_critical_str))
         sys.exit(2)
     elif len(folder_warning) > 0:
         folder_warning_str = separator.join(folder_warning)
-        print(f'WARNING: folder_id:bytes_behind - {folder_warning_str}')
+        print('WARNING: folder_id:bytes_behind - '
+              '{}'.format(folder_warning_str))
         sys.exit(1)
     else:
-        print(f"OK: All folders are 'UP to Date'")
+        print("OK: All folders are 'UP to Date'")
         sys.exit(0)
 
 
@@ -200,9 +207,9 @@ def action_to_do(action, http_endpoint, headers):
 
 action = args['action']
 if args['https']:
-    http_endpoint = f"https://{args['host']}:{args['port']}"
+    http_endpoint = 'https://{}:{}'.format(args['host'], args['port'])
 else:
-    http_endpoint = f"http://{args['host']}:{args['port']}"
+    http_endpoint = 'http://{}:{}'.format(args['host'], args['port'])
 headers = {'X-API-Key': args['api_key']}
 
 action_to_do(action, http_endpoint, headers)
